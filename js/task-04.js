6f4d12639743b5fd08f7d331b619470495342989
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-15 20:43:22 +0200

    Вирішує 4 задачу

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,25 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const buttonRefs = {
+    reduceButton: document.querySelector("[data-action='decrement']"),
+    increaseButton: document.querySelector("[data-action='increment']"),
+}
+// console.log(buttonRefs.reduceButton);
+// console.log(buttonRefs.increaseButton);
+
+buttonRefs.reduceButton.addEventListener("click", onReduceButtonClick);
+buttonRefs.increaseButton.addEventListener("click", onIncreaseButtonClick);
+
+let counterValue = 0;
+
+function onReduceButtonClick(){
+	counterValue -=1;
+  document.getElementById('value').innerHTML = counterValue;
+}
+
+function onIncreaseButtonClick(){
+	counterValue +=1;
+  document.getElementById('value').innerHTML = counterValue;
+}
