93e33ee787622d94c1c8aa98f89aae144167c8ec
TamaraProkhorova <99213837+TamaraProkhorova@users.noreply.github.com>
2023-01-15 21:03:20 +0200

    Вирішує 5 задачу

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,13 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, 
-// у спані повинен відображатися рядок "Anonymous".
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне 
+// значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+const nameInput = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+// console.log(nameInput);
+// console.log(nameOutput);
+
+nameInput.addEventListener("input", (event) => {
+   nameOutput.textContent = event.currentTarget.value || "Anonymous";
+});
+
